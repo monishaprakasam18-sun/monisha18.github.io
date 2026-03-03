1e1a50e0bb196d1db332d77a1e0718b7ad1834f6
Monisha.Github.i <monishaprakasam18@gmail.com>
2026-03-03 18:40:28 +0530

    Create particles.js

---
+++ particles.js
@@ -0,0 +1,11 @@
+/* ---------------------------------------------------
+PARTICLES.JS LOADER (SAFE FOR GITHUB PAGES)
+--------------------------------------------------- */
+
+particlesJS.load(
+"particles-js",        // The ID of the container
+"particles.json",       // The config file (must be in same folder)
+function () {
+console.log("Floating particles loaded successfully.");
+}
+);
